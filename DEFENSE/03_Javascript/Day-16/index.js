430e6c329107abc2a8a8cba390a0f2813c047d25
Anushka Jachak <anushkajachak11@gmail.com>
2025-10-18 16:26:40 +0530

    js started

---
+++ DEFENSE/03_Javascript/Day-16/index.js
@@ -0,0 +1,47 @@
+//variables kaise banate hai
+
+// let name = "Manu";
+// let age =  5;
+
+// age = 7;
+// console.log(name ,age);
+
+// const account = 70000;
+// account = 100000;   //Error
+// console.log(account)
+
+
+//old tarika
+
+// var a = 10;
+// var a = 20;
+// var a = 30;
+// var a = 40;
+// var a = 50;
+// console.log(a);
+
+
+        /*Primitive data type*/
+//number
+let a = 24;
+let b = 60;
+console.log(a,b);
+
+//string 
+let c = 'Strike is coming';
+let d = 'Anushka';
+console.log(c,d);
+
+//boolean
+let login = true;
+let f = false;
+console.log(login,f);
+
+//undefined
+let user;
+// const p;
+console.log(user);
+
+
+
+
